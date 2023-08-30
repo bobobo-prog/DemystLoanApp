0b17a1af976ca657ef97c3080557317efe78ebf1
bobobo-prog <ashwin.mp@somaiya.edu>
2023-08-30 23:57:49 +0530

    first commit

---
+++ src/test/java/com/example/demystloanapp/DemystLoanApp/DemystLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demystloanapp.DemystLoanApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemystLoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
